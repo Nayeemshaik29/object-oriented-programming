4bf9738fe4349c379a4ff067cc6a49c2f4087772
Nayeemshaik29 <bashanayeem79@gmail.com>
2026-02-05 13:50:39 +0530

    Basic concepts covered like Class , Objects and Constructors

--- src/com/java/oop/Product.java
+++ src/com/java/oop/Product.java
@@ -1,29 +1,76 @@
 package com.java.oop;
 
 public class Product {
-    int productId;
-    String productName;
-    int maxRetialPrice;
-    float stateTax;
+    int id;
+    String name;
+    float maxRetailPrice;
     float centralTax;
-    float discount;
-    int quantity;
-    long stock;
+    float stateTax;
+    float discountPercentage;
+    float discountAmount;
+    float finalPrice;
+    int shippingCharges;
+
     public Product() {
-        System.out.println("Product Constructor... Has initialized");
+        id = 123;
+        name = "dell laptop";
+        maxRetailPrice = 50000.00F;
+        centralTax = 2.5F;
+        stateTax = 2.5F;
+        discountPercentage = 7.5F;
+        shippingCharges=100;
+
     }
-    public Product(int productId, String productName,int maxRetialPrice, float stateTax, float centralTax, float discount, int quantity, long stock) {
-        this.productId = productId;
-        this.productName = productName;
-        this.maxRetialPrice = maxRetialPrice;
-        this.stateTax = stateTax;
+
+    public Product(int id, String name, float maxRetailPrice, float centralTax, float stateTax, float discountPercentage, float discountAmount, float finalPrice, int shippingCharges) {
+        this.id = id;
+        this.name = name;
+        this.maxRetailPrice = maxRetailPrice;
         this.centralTax = centralTax;
-        this.discount = discount;
-        this.quantity = quantity;
-        this.stock = stock;
+        this.stateTax = stateTax;
+        this.discountPercentage = discountPercentage;
+        this.discountAmount = discountAmount;
+        this.finalPrice = finalPrice;
+        this.shippingCharges = shippingCharges;
+    }
 
+    public void displayProduct() {
+        System.out.println("Product ID : " + id);
+        System.out.println("Product Name : " + name);
+        System.out.println("Product maxRetailPrice : " + maxRetailPrice);
+        System.out.println("Discount Amount : "+calculateDiscount());
+        System.out.println("Price after Discount : "+priceAfterDiscount());
+        System.out.println("centralTax : " + centralTax);
+        System.out.println("StateTax : " + stateTax);
+        System.out.println("Shipping charges : " + shippingCharges);
+        System.out.println("Final Bill : "+calculateFinalPrice());
+
+    }
+
+    float calculateDiscount() {
+        return maxRetailPrice * discountPercentage / 100;
+
+    }
+
+    float priceAfterDiscount() {
+        return maxRetailPrice - calculateDiscount();
+
+    }
+
+    float calculateStateTax() {
+        return priceAfterDiscount() * stateTax / 100;
+    }
+
+    float calculateCentralTax() {
+        return priceAfterDiscount() * centralTax / 100;
+    }
+
+    float calculateFinalPrice() {
+        return priceAfterDiscount()+calculateStateTax()+calculateCentralTax() + shippingCharges;
 
     }
 
 
+    public void display() {
+    }
 }
